56ab964f9999db16b95fe01c03f8e55541909928
时金盈 <jy_shi@163.com>
2018-03-26 23:47:47 +0800

    固定 footer 页面底部

--- src/static/my.js
+++ src/static/my.js
@@ -67,4 +67,48 @@ $(document).ready(function(){
       $('#is_public').removeProp('checked');
     }
   // });
+
+    // $('.dropdown-item').click(function () {
+    //     $('.dropdown-item').removeClass('active');
+    //     $(this).addClass('active')
+    // })
+});
+
+//加载后自动执行
+$(function() {
+
+    // 设置图口高度，此处让 footer 自动填高
+    // $(window).resize(function() {
+    //     $('div:last').height($(window).height() - $('div:last').offset().top);
+    // });
+    // $(window).resize();
+
+    // 修改所有外链接的打开方式：在浏览器的新窗口中打开
+    $(document.links).filter(function() {
+        return this.hostname != window.location.hostname;
+    }).attr('target', '_blank');
+
+});
+
+
+$(document).ready(function() {
+
+ // var docHeight = $(window).height();
+ // var footerHeight = $('footer').height();
+ // var footerTop = $('footer').position().top + footerHeight;
+ //
+ // if (footerTop < docHeight) {
+ //  $('footer').css('margin-top', 10 + (docHeight - footerTop - 106) + 'px');
+ // }
+
+//  if ($(document.body).height() < $(window).height()) {
+//   $('footer').attr('style', 'position: fixed!important; bottom: 0px;');
+// }
+
+    // body的padding为56,所以减去56
+    if ($(window).height() > $('body').height())
+        {
+        var extra = $(window).height() - $('body').height() - 56;
+        $('footer').css('margin-top', extra);
+        }
 });
